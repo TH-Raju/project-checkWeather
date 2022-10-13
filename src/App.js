e4681ac12c5f2b30878a0e9e87a41548d4ced96a
TH-Raju <rajukhan894200@gmail.com>
2022-10-13 19:13:16 +0600

    background added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
+import { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
 function App() {
   const [query, setQuery] = useState('');
+  const search = async (e) => {
+    if (e.key === 'Enter') {
+      const data = await fetchWeather(query)
+      console.log(data)
+    }
+  }
+
+
+
   return (
     <div className='main-container'>
       <input
@@ -11,6 +21,7 @@ function App() {
         placeholder="Search"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
       />
     </div>
   );
