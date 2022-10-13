cd5b6a784847b7d040b54f9e2f5a78d925f14038
TH-Raju <rajukhan894200@gmail.com>
2022-10-13 19:26:37 +0600

    description added

--- src/App.js
+++ src/App.js
@@ -30,6 +30,10 @@ function App() {
             {Math.round(weather.main.temp)}
             <sup>&deg;C</sup>
           </div>
+          <div className="info">
+            <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+            <p>{weather.weather[0].description}</p>
+          </div>
         </div>
       )}
     </div>
